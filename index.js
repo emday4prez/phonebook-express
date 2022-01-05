74aaea0e2adad4baaa039987d5592e3f51f0c34c
Emerson <emerson.day@gmail.com>
2022-01-04 16:56:02 -0800

    REST practice

--- index.js
+++ index.js
@@ -3,18 +3,8 @@ const app = express()
 
 app.use(express.json())
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`phonebook server running on port ${PORT}`)
 
-
-
-
-
-
-
-
-const persons = [
+let persons = [
     { 
       "id": 1,
       "name": "Arto Hellas", 
@@ -36,3 +26,68 @@ const persons = [
       "number": "39-23-6423122"
     }
 ]
+
+
+app.get('/api/persons', (request, response) => {
+ response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  const body = `<p>Phonebook has info for ${persons.length} people.</p><p>${new Date()}</p>`
+  response.status(200).send(body)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+ const id = +request.params.id
+ const person = persons.filter(person => person.id === id)
+
+ if(person){
+  response.json(person)
+ }else{
+  response.status(404).end()
+ }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+ const id = +request.params.id
+ persons = persons.filter(person => person.id !== id)
+ response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+ 
+ const body = request.body
+
+ if(!body.name){
+  return response.status(400).json({
+   error: 'name required'
+  })
+ }if(!body.number){
+  return response.status(400).json({
+   error: 'number required'
+  })
+ } 
+ function userExists(name) {
+  return persons.some(function(el) {
+    return el.name === name;
+  }); 
+ }
+ if(userExists(body.name)){
+  return response.status(400).json({
+   error: "that person is already in the phonebook"
+  });
+ }
+
+ const person = {
+  
+  id: Math.floor(Math.random() * 10000),
+  name: body.name,
+  number: body.number
+ }
+ persons = persons.concat(person)
+ response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`phonebook server running on port ${PORT}`)
